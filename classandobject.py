ef6b4fcca928a2f237f3d3f8de6fd4852f71c391
Denish Borad <denish5106@gmail.com>
2022-12-26 10:40:29 +0530

    commite
    initial

---
+++ classandobject.py
@@ -0,0 +1,10 @@
+class math:
+    def fun(x,y):
+        print("sum",x+y)
+        print("sub",x-y)
+        print("mul",x*y)
+        print("div",x/y)
+
+Sum = math   #Sum is object
+# Sum.fun(50,20)
+# Sum.fun(30,5)
---
+++ modules.py
@@ -0,0 +1,9 @@
+from classandobject import math
+
+class cal:
+    def get():
+        print("calculation")
+
+calcu = cal
+calcu.get()
+math.fun(50,40)
