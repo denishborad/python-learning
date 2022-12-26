8674c69b8372f7a53fa4c7fa26e3ff32809346c6
Denish Borad <denish5106@gmail.com>
2022-12-26 10:57:17 +0530

    commite
    initial

---
+++ multipleclassandobject.py
@@ -0,0 +1,24 @@
+class sum:
+    def fun(x,y):
+        print("sum",x+y)
+Sum = sum   #Sum is object
+# Sum.fun(50,20)
+# Sum.fun(30,5)
+
+class sub:
+    def subtract(x,y):
+        print("sub",x-y)
+Sub = sub
+# Sub.subtract(50,10)
+
+class mul:
+    def multiply(x,y):
+        print("mul",x*y)
+Mul = mul
+# Mul.multiply(50,2)
+
+class div:
+    def divide(x,y):
+        print("div",x/y)
+Div = div
+# Div.divide(50,2)
---
+++ multiplyfuncinmuldules.py
@@ -0,0 +1,13 @@
+from multipleclassandobject import sum,sub,mul,div
+
+x=int(input("Number1: "))
+y=int(input("Number2: "))
+class cal:
+    def get():
+        print("Calculation")
+calcu = cal
+calcu.get()
+sum.fun(x,y)
+sub.subtract(x,y)
+mul.multiply(x,y)
+div.divide(x,y)
